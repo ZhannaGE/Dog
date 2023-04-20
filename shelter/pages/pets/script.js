4e1bfaada3b6ea93e2429bc05a1536c928ed3c46
Zhanna Eloshenko <eloshenko.zhanna@gmaail.com>
2023-04-20 14:26:41 +0300

    deleted json file

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -30,81 +30,81 @@ function burgerMenu() {
 }
 
 burgerMenu();
-let SCREEN_WIDTH = 0;
 
-function setScreenWidth() {
+let SCREEN_WIDTH = 0; // 1 создаем переменную текущей ширины
+
+function setScreenWidth() { // 2 кладем в нее ширину получаемую в зависимости от экрана
 
     if (window.innerWidth < 768) {
         SCREEN_WIDTH = 0;
-    }
-    else if(window.innerWidth > 768 && window.innerWidth < 1100) {
+    } else if (window.innerWidth > 768 && window.innerWidth < 1100) {
         SCREEN_WIDTH = 768;
-    }
-    else {
+    } else {
         SCREEN_WIDTH = 1100;
     }
 }
+
 setScreenWidth();
 
-function pagination(selector, limitOptions, startPage = 1) {
-    // Необязательные параметры (верс я 1)
-    // if(startPage) {
-    //     thisPage = startPage
-    // } else {
-    //     thisPage = 1;
-    // }
+const responsiveLimits = { // задаем объект с количеством видимых сдайдов
+    0: {
+        limit: 3,
+    },
+    768: {
+        limit: 3,
+    },
+    1100: {
+        limit: 8,
+    }
+};
 
-    // Необязательные параметры (верс я 2)
-    //let thisPage = startPage || 1;
+function pagination(selector, limitOptions, startPage = 1) { //функция которая принимает селекторы, наш объект и текущую страницу
 
 
-
-    function loadItem(){
+    function loadItem() { // вычитываем кол показываемых слайдов и меняем класс в зависимости от числа
         let beginGet = limit * (thisPage - 1);
         let endGet = limit * thisPage - 1;
-        list.forEach((item, key)=>{
-            if(key >= beginGet && key <= endGet){
+        list.forEach((item, key) => {
+            if (key >= beginGet && key <= endGet) {
                 item.style.display = 'block';
-            }else{
+            } else {
                 item.style.display = 'none';
             }
         })
         listPage();
     }
 
-    function listPage(){
+
+    function listPage() {
         let count = Math.ceil(list.length / limit);
+        console.log(count)
         document.querySelector('.listPage').innerHTML = ''; // чистка ппагинации от старых страниц (кнопок на другие страницы)
 
-        if(thisPage != 1){
+        if (thisPage !== 1) {
             let prev = document.createElement('li');
-            prev.innerText = 'PREV';
-            //prev.setAttribute('onclick', "changePage(" + (thisPage - 1) + ")");
+            prev.innerText = '<<';
             prev.addEventListener('click', function () {
                 changePage(thisPage - 1)
             })
             document.querySelector('.listPage').appendChild(prev);
         }
 
-        for(let i = 1; i <= count; i++){
+        for (let i = 1; i <= count; i++) {
             let newPage = document.createElement('li');
             newPage.dataset.pageNumber = i;
             newPage.innerText = i;
-            if(i == thisPage){
+            if (i === thisPage) {
                 newPage.classList.add('active');
             }
-            // newPage.setAttribute('onclick', "changePage(" + i + ")");
             newPage.addEventListener('click', function () {
                 changePage(i)
             })
             document.querySelector('.listPage').appendChild(newPage);
         }
 
-        if(thisPage != count){
+        if (thisPage !== count) {
             let next = document.createElement('li');
-            next.innerText = 'NEXT';
-            // next.setAttribute('onclick', "changePage(" + (thisPage + 1) + ")");
-
+            next.innerText = '>>';
             next.addEventListener('click', function () {
                 changePage(thisPage + 1)
             })
@@ -112,7 +112,7 @@ function pagination(selector, limitOptions, startPage = 1) {
         }
     }
 
-    function changePage(i){
+    function changePage(i) {
         thisPage = i;
         loadItem();
     }
@@ -120,8 +120,11 @@ function pagination(selector, limitOptions, startPage = 1) {
     let thisPage = startPage;
     let list = document.querySelectorAll(selector);
     let limit = limitOptions[SCREEN_WIDTH].limit;
+    console.log(list)
 
     loadItem();
+
+// не пойму зачем это
 
     document.body.addEventListener('click', function (e) {
         const listPage = document.querySelector('.listPage');
@@ -139,37 +142,13 @@ function pagination(selector, limitOptions, startPage = 1) {
 
 }
 
+pagination('.card-container .card-pet', responsiveLimits);
 
-
-// function pagination2(setting) {
-//     console.log(setting.selector)
-// }
-//
-// const pag2Setings = {
-//     selector:'.card-container .card-pet' ,
-//     limit: 4,
-//     startPage: 2
-// }
-
-const responsiveLimits = {
-    0: {
-        limit: 1,
-    },
-    768: {
-        limit: 3,
-    },
-    1100:{
-        limit: 8,
-    }
-};
-
-pagination('.card-container .card-pet',responsiveLimits, 2);
-
-window.addEventListener('resize', function (){
+window.addEventListener('resize', function () {
     console.log('Здесьмы будем перерисовывать пагинцаию');
     setScreenWidth();
     console.log(SCREEN_WIDTH)
-    pagination('.card-container .card-pet',responsiveLimits);
+    pagination('.card-container .card-pet', responsiveLimits);
 })
 
 
